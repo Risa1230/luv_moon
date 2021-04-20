c3d1be1d51f9e8759979daefc524d19711978231
Risa Sakurai <82878274+Risa1230@users.noreply.github.com>
2021-04-20 21:33:08 +0900

    Create MyFavorite.java

---
+++ MyFavorite.java
@@ -0,0 +1,4 @@
+public class MyFavorite {
+  public static void main (String[] args) {
+    System.out.println("私の好きな食べ物はお寿司です。");
+    System.out.println("理由は、単純にとても美味しいと思っているからです。毎食食べてもいいと思う程好きです。和食は全般好きですが、特に生魚の新鮮さとシャリとの相性の良さによって他の食べ物よりも特別美味しいと思うのだと思います。");
